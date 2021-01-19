a4cc62d07d3f3853c1ca3af2a31b33e47259afcb
JEROME Emilien <jerome.emi@onja.org>
2021-01-19 12:00:01 +0300

    dispaly the data from jumbo.json and style the all of the components element from containers folder in jumbotron file inside styles folder

--- src/components/jambotron/index.js
+++ src/components/jambotron/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import {Item, Inner, Container, Title, SubTitle, Image, Pane} from './styles/jumbotron';
 
-
-export default function  jumbotron({
+export default function  Jumbotron({
     children,
     direction= 'row',
     ...restProps}) {
@@ -12,4 +12,24 @@ export default function  jumbotron({
             </Inner>
         </Item>
     )
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumbotronPane({children, ...restProps}) {
+    return <Pane {...restProps}>{children}</Pane>;
+}
+
+Jumbotron.Title = function JumbotronTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>;
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({children, ...restProps}) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumbotronImage({children, ...restProps}) {
+    return <Image {...restProps}/>;
 }
